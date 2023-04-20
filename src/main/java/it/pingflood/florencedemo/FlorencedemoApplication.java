ed9d18c40e32817e7ea3a2231a1904111e2f6efa
Paola Guarasci <paolaguarasci@gmail.com>
2023-04-20 20:57:25 +0200

    first commit

---
+++ src/main/java/it/pingflood/florencedemo/FlorencedemoApplication.java
@@ -0,0 +1,13 @@
+package it.pingflood.florencedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlorencedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlorencedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/pingflood/florencedemo/FlorencedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pingflood.florencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlorencedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
